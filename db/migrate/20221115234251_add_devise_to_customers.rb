a9ea6e81c43c6c2b164f61d38214a3fe705343ae
EC2 Default User <ec2-user@ip-172-31-33-190.ap-northeast-1.compute.internal>
2022-11-28 15:33:50 +0000

    Bootstrap導入

--- db/migrate/20221115234251_add_devise_to_customers.rb
+++ db/migrate/20221115234251_add_devise_to_customers.rb
@@ -7,7 +7,7 @@ class AddDeviseToCustomers < ActiveRecord::Migration[6.1]
       t.string :last_name, null:false
       t.string :first_name, null:false
       t.string :last_name_kana, null:false
-      t.string :first_name_kara, null:false
+      t.string :first_name_kana, null:false
       t.string :email,              null:false, default: ""
       t.string :encrypted_password, null:false, default: ""
       t.string :postal_code, null:false
--- db/schema.rb
+++ db/schema.rb
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2022_11_17_052824) do
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
-    t.string "first_name_kara", null: false
+    t.string "first_name_kana", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "postal_code", null: false
